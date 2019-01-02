f010fb36dd497deefcaf50ca6d4ed184ed3e4677
SK00587572 <SK00587572@INHYTCPC07918.TechMahindra.com>
2019-01-02 10:19:10 +0530

    first change

---
+++ src/main/java/com/example/shruthi/shruthigradle/ShruthiGradleApplication.java
@@ -0,0 +1,14 @@
+package com.example.shruthi.shruthigradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShruthiGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShruthiGradleApplication.class, args);
+	}
+
+}
+
